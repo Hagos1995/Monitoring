5ef15b409b1e51401a8a612ba88985da6bd8aaf2
Hesse <Marc.Hesse@wvsi.local>
2019-04-04 12:15:33 +0200

    test2

---
+++ iF else.ps1
@@ -0,0 +1,9 @@
+﻿New-Variable Schall -Value 342.2 -Description "Schallgeschwindigkeit " -Option ReadOnly
+do {
+if ($Dauer -match '^\d+$' ) { Die Eingabe ist Falsch }
+$Dauer = Read-Host -Prompt "Bitte geben sie die zeit an die der Donner nach dem Blitz kam ." }
+ until ($Dauer -match '^\d+$' ) 
+[ double ] $D = $Dauer
+$Entfernung = $D * $Schall
+Write-Host "Die Entfernung des Blitzes beträgt $Entfernung Meter ." 
+Remove-Variable Schall -Force
